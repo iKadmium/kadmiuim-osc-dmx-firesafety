80ce5f9d6ecae5b03814d031f7fddaa1971482ef
iKadmium <kadmium@gmail.com>
2018-02-22 21:45:29 +1000

    Initial commit - nothing works yet

---
+++ kadmium-osc-dmx-firesafety/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace kadmium_osc_dmx_firesafety
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer updateTimer;
+        TimeSpan updateTime = new TimeSpan(0, 0, 0, 0, 250);
+        EventHandler updateHandler;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            updateHandler = new EventHandler(onUpdate);
+            updateTimer = new DispatcherTimer(updateTime, DispatcherPriority.Normal, onUpdate, Dispatcher);
+        }
+
+        private void onUpdate(object caller, EventArgs args)
+        {
+            string bpb = "";
+            string fred = "";
+        }
+        
+    }
+}
